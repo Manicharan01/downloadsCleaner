e9e9f1743b832ce07c7d8dac3c2ff920c2b3d3ed
Manicharan <manicharan150@gmail.com>
2024-08-22 05:01:55 +0530

    Feat: can move file to their folders

--- mover.py
+++ mover.py
@@ -1,24 +1,54 @@
 import os
 
-download_path = "/home/charan/Downloads"
+download_path = "/home/charan/Downloads"  # The directory we want to work on
+
+# This sets the given directory as working, so that we can maniupulte said directory
 os.chdir(download_path)
-print("In:", os.getcwd())
+
+# Dict Initalizing
+fileAndFolderMapper = {
+    "dirs": [],
+    "music": [],
+    "video": [],
+    "image": [],
+    "misc": [],
+}
+
+
+# To populate the fileAndFolderMapper dict with the files according to their extension type
+def mapperPopulator(files):
+    for file in files:
+        if os.path.isdir(download_path + "/" + file):
+            fileAndFolderMapper["dirs"].append(file)
+
+        elif os.path.isfile(download_path + "/" + file):
+            extension = os.path.splitext(file)[1]
+            if extension == ".png" or extension == ".jpg":
+                fileAndFolderMapper["image"].append(file)
+            elif extension == ".mp4" or extension == ".mkv":
+                fileAndFolderMapper["video"].append(file)
+            elif extension == ".mp3":
+                fileAndFolderMapper["music"].append(file)
+
+
+# To populate the files list with the files available is the Downloads directory
 files = os.listdir()
 
-fileAndFolderMapper = {"dirs": [], "music": [], "video": [], "image": [], "misc": []}
 
-for file in files:
-    if os.path.isdir(download_path + "/" + file):
-        fileAndFolderMapper["dirs"].append(file)
+# Gives control to mapperPopulator to populate the dict with files available in directory
+if files:
+    mapperPopulator(files)
 
-    elif os.path.isfile(download_path + "/" + file):
-        extension = os.path.splitext(file)[1]
-        print(extension)
-        if extension == ".png" or extension == ".jpg":
-            fileAndFolderMapper["image"].append(file)
-        elif extension == ".mp4" or extension == ".mkv":
-            fileAndFolderMapper["video"].append(file)
-        elif extension == ".mp3":
-            fileAndFolderMapper["music"].append(file)
 
-print(fileAndFolderMapper)
+# This runs until list available in fileAndFolderMapper are empty
+while True:
+    for file in fileAndFolderMapper["music"]:
+        os.system("mv" + " '" + file + "' " + "/home/charan/Music")
+    fileAndFolderMapper["music"].clear()
+    for file in fileAndFolderMapper["video"]:
+        os.system("mv" + " '" + file + "' " + "/home/charan/Videos")
+    fileAndFolderMapper["video"].clear()
+    for file in fileAndFolderMapper["image"]:
+        os.system("mv" + " '" + file + "' " + "/home/charan/Pictures")
+    fileAndFolderMapper["image"].clear()
+    break
